b6cb48d5e5b3663b413709170a40383783652739
main() <main@ehvag.de>
2014-10-29 16:20:36 +0100

    Initial commit

---
+++ SyslogTrace/NativeString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace SyslogTrace
+{
+	public class NativeString : IDisposable
+	{
+		private readonly byte[] Chars;
+		private GCHandle CharHandle;
+		public IntPtr Pointer { get { return CharHandle.AddrOfPinnedObject(); } }
+		public bool Disposed { get { return !CharHandle.IsAllocated; } }
+
+		public NativeString(string s)
+		{
+			Chars = Encoding.ASCII.GetBytes(s + "\0");
+			CharHandle = GCHandle.Alloc(Chars, GCHandleType.Pinned);
+		}
+
+		~NativeString()
+		{
+			Dispose();
+		}
+
+		public void Dispose()
+		{
+			if (CharHandle.IsAllocated)
+				CharHandle.Free();
+		}
+	}
+}
+
---
+++ SyslogTrace/SyslogTraceListener.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using Mono.Unix.Native;
+
+namespace SyslogTrace
+{
+	public class SyslogTraceListener : TraceListener
+	{
+		private readonly NativeString LoggerName;
+
+		public SyslogTraceListener(string name)
+		{
+			LoggerName = new NativeString(name);
+			Syscall.openlog(LoggerName.Pointer, SyslogOptions.LOG_PID, SyslogFacility.LOG_DAEMON);
+		}
+
+		~SyslogTraceListener()
+		{
+			Dispose();
+		}
+
+		public void DisposeThis()
+		{
+			if (!LoggerName.Disposed) {
+				WriteLine("going down", "SyslogTraceListener");
+				Syscall.closelog();
+				LoggerName.Dispose();
+			}
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			DisposeThis();
+			base.Dispose(disposing);
+		}
+
+		public override bool IsThreadSafe { get { return true; } }
+
+		public override void Write(string message)
+		{
+			WriteLine(message);
+		}
+
+		public override void WriteLine(string message)
+		{
+			Syscall.syslog(SyslogLevel.LOG_INFO, message);
+		}
+	}
+}
+
